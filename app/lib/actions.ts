c8e09f787a5691fc17adba84398f1724cb71d5fa
erwinnp <moongraph21@gmail.com>
2023-11-13 01:25:39 +0700

    chapter 15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -114,3 +115,17 @@ export const deletInvoice = async (id: string) => {
     };
   }
 };
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialsSignin";
+    }
+    throw error;
+  }
+}
